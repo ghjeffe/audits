5e1986442aa9858194a3060b92f26f33b478c575
ghjeffe <garyjeffersii@gmail.com>
2016-07-21 00:07:21 -0500

    initial

---
+++ sglpt_cck.py
@@ -0,0 +1,29 @@
+#!python3
+
+from collections import namedtuple
+
+def parse_svc_file():
+    CSV_Parse = namedtuple('CSV_Parse', ['service'
+                                        ,'acct_name'
+                                        ,'acct_num'
+                                        ,'bank_name'
+                                        ,'bank_aba'
+                                        ,'users'
+                                        ]
+                           )
+    
+    with open('samples/ServiceProfileReport07202016223214.CSV') as svc_rpt:
+        for line in svc_rpt:
+            line = line.strip('\n')
+            try:
+                parsed = CSV_Parse(*line.split('","'))
+            except TypeError: #.split() throws TypeError if delimiter not found
+                pass
+            else:
+                users = [user.strip('\n" ') for user in parsed.users.split(', ')]
+                try:
+                    users.remove('')
+                except ValueError:
+                    pass
+                if len(users) > 0:
+                    print(parsed.service, parsed.acct_name, parsed.users, sep='||')
